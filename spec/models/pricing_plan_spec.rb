e5dba07072e7a833e967ce883c50dda23b9e2776
Jaime Alvarez <jaime_andres@unitec.edu>
2016-07-26 05:38:35 +0000

    Pricing Plan model spec

--- spec/models/pricing_plan_spec.rb
+++ spec/models/pricing_plan_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe PricingPlan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "has a valid factory" do
+        expect(FactoryGirl.create(:pricing_plan)).to be_valid 
+    end
+    
+    it "is invalid without a name" do
+        expect(FactoryGirl.build(:pricing_plan, name: nil)).to_not be_valid 
+    end
+    
+    it "is invalid without a price" do
+        expect(FactoryGirl.build(:pricing_plan, price: nil)).to_not be_valid 
+    end
+    
+    it "is invalid without a state" do
+        expect(FactoryGirl.build(:pricing_plan, state: nil)).to_not be_valid 
+    end
 end
