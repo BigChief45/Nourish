588b9263eb972a9913f571782cc767d348f225ef
Jaime Alvarez <jaime_andres@unitec.edu>
2016-07-26 05:40:37 +0000

    Change tests to use expect syntax

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
     it "has a valid factory" do
-        FactoryGirl.create(:user).should be_valid 
+        expect(FactoryGirl.create(:user)).to be_valid 
     end
     
     it "is invalid without an email" do
-        FactoryGirl.build(:user, email: nil).should_not be_valid
+        expect(FactoryGirl.build(:user, email: nil)).to_not be_valid
     end
     
     it "is invalid without a password" do
-        FactoryGirl.build(:user, password: nil).should_not be_valid
+        expect(FactoryGirl.build(:user, password: nil)).to_not be_valid
     end
     
     it "is invalid without matching passwords" do
-        FactoryGirl.build(:user, password: 'password', password_confirmation: 'please').should_not be_valid
+        expect(FactoryGirl.build(:user, password: 'password', password_confirmation: 'please')).to_not be_valid
     end
         
     # Pricing Plan
