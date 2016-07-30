165ec9b88c9584ed844d4dedad03a457173206d3
Jaime Alvarez <jaime_andres@unitec.edu>
2016-07-30 07:31:31 +0000

    Orders controllers and views

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,27 @@
 class OrdersController < ApplicationController
     
+    respond_to :html
+    
     before_action :authenticate_user!
+    load_and_authorize_resource
+    
+    before_action :find_order, only: [:show]
     
     def index
         @orders = Order.all.order('created_at DESC') 
     end
+    
+    def show
+    end
+    
+    def destroy
+        @order.destroy
+        respond_with @order, location: -> { orders_path }
+    end
+    
+    private
+    
+        def find_order
+            @order = Order.find(params[:id])
+        end
 end
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,23 @@
 module OrdersHelper
+    
+    def display_order_status status
+        case status
+            when "pending"
+                content_tag :span, :class => "label label-warning" do
+                    content_tag(:i, '', :class => "fa fa-clock-o") + " #{status.titleize}"
+                end
+            when "delivering"
+                content_tag :span, :class => "label label-info" do
+                    content_tag(:i, '', :class => "fa fa-motorcycle") + " #{status.titleize}"
+                end
+            when "completed"
+                content_tag :span, :class => "label label-success" do
+                    content_tag(:i, '', :class => "fa fa-check") + " #{status.titleize}"
+                end
+            when "cancelled"
+                content_tag :span, :class => "label label-danger" do
+                    content_tag(:i, '', :class => "fa fa-times") + " #{status.titleize}"
+                end
+        end
+    end
 end
